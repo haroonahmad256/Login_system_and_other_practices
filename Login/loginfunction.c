ed3e9a35bb32da1092e77592ce433c21c399f601
haroonahmad256 <ha9417293@gmail.com>
2025-12-16 17:54:44 +0500

    Login system using file handling

--- Login/loginfunction.c
+++ Login/loginfunction.c
@@ -10,103 +10,122 @@ void signup_function(char firstname[], char lastname[], char email[], char usern
     fprintf(fptr, "%s %s %s %s %s %s\n", firstname, lastname, email, username, phonenumber, password);
     fclose(fptr);
 }
-void signin_function(char input_username[], char input_password[])
+
+void signin_function()
 {
+    int choice;
+    printf("1. Enter information to login\n");
+    printf("2. Reset Password\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
 
-    FILE *fptr;
-    fptr = fopen("users.txt", "r");
-
-    if (fptr == NULL)
+    switch (choice)
     {
-        printf("Error opening file");
-    }
+    case 1:
+        FILE *fptr;
+        fptr = fopen("users.txt", "r");
 
-    int i = 1;
-    do
-    {
-        int on = 0;
+        if (fptr == NULL)
+        {
+            printf("Error opening file");
+        }
+        char enter_username[50];
+        char enter_password[20];
+        printf("Enter your username: ");
+        scanf("%s", enter_username);
+        printf("Enter your password: ");
+        scanf("%s", enter_password);
+
+        int i = 1;
+        do
+        {
+            int on = 0;
+            char username[20];
+            char password[20];
+            char firstname[20], lastname[20], email_address[100], phonenumber[20];
+            char new_password[20];
+
+            while (fscanf(fptr, "%s %s %s %s %s %s", firstname, lastname, email_address, username, phonenumber, password) == 6)
+            {
+                // fscanf(fptr,"%s %d",username, &password );
+                if (strcmp(username, enter_username) == 0 && strcmp(password, enter_password) == 0)
+                {
+                    on = 1;
+                }
+            }
+
+            if (on)
+            {
+                printf("Login successful\n");
+                i = 0;
+            }
+
+            else
+            {
+                printf("Invaid username or password\n");
+                break;
+            }
+            fclose(fptr);
+
+        } while (i);
+        
+        break;
+    
+    case 2:
+        rewind(fptr);
+        FILE *fptr2;
+        fptr = fopen("users.txt", "r");
+        fptr2 = fopen("user_temp.txt", "w");
         char username[20];
         char password[20];
         char firstname[20], lastname[20], email_address[100], phonenumber[20];
         char new_password[20];
+        char input_email[100];
+        char fpassword[1];
+        printf("Forgot password(y/n): ");
+        scanf("%s", fpassword);
+        int inside = 0;
 
-        while (fscanf(fptr, "%s %s %s %s %s %s", firstname, lastname, email_address, username, phonenumber, password) == 6)
+        if (fpassword[0] == 'y' || fpassword[0] == 'Y')
         {
-            // fscanf(fptr,"%s %d",username, &password );
-            if (strcmp(username, input_username) == 0 && strcmp(password, input_password) == 0)
+            printf("Enter your email address to verify: ");
+            scanf("%s", input_email);
+
+            while (fscanf(fptr, "%s %s %s %s %s %s", firstname, lastname, email_address, username, phonenumber, password) == 6)
             {
-                on = 1;
+
+                if (strcmp(email_address, input_email) == 0)
+                {
+                    printf("Enter new password: ");
+                    scanf("%s", new_password);
+                    fprintf(fptr2, "%s %s %s %s %s %s\n", firstname, lastname, email_address, username, phonenumber, new_password);
+                    inside = 1;
+                }
+                else
+                {
+
+                    fprintf(fptr2, "%s %s %s %s %s %s\n", firstname, lastname, email_address, username, phonenumber, password);
+                }
             }
-        }
 
-        if (on)
-        {
-            printf("Login successful\n");
-            i = 0;
-        }
+            fclose(fptr2);
+            fclose(fptr);
 
-        else
-        {
-            printf("Invaid username or password\n");
-            break;
-        }
-        fclose(fptr);
-
-    } while (i);
-}
-
-void forgot__reset_password()
-{
-    FILE *fptr;
-    FILE *fptr2;
-    fptr = fopen("users.txt", "r");
-    fptr2 = fopen("user_temp.txt", "w");
-    char username[20];
-    char password[20];
-    char firstname[20], lastname[20], email_address[100], phonenumber[20];
-    char new_password[20];
-    char input_email[100];
-    char fpassword[1];
-    printf("Forgot password(y/n): ");
-    scanf("%s", fpassword);
-    int inside = 0;
-
-    if (fpassword[0] == 'y' || fpassword[0] == 'Y')
-    {
-        printf("Enter your email address to verify: ");
-        scanf("%s", input_email);
-
-        while (fscanf(fptr, "%s %s %s %s %s %s", firstname, lastname, email_address, username, phonenumber, password) == 6)
-        {
-
-            if (strcmp(email_address, input_email) == 0)
+            if (inside)
             {
-                printf("Enter new password: ");
-                scanf("%s", new_password);
-                fprintf(fptr2, "%s %s %s %s %s %s\n", firstname, lastname, email_address, username, phonenumber, new_password);
-                inside = 1;
+                remove("users.txt");
+                rename("user_temp.txt", "users.txt");
+                printf("Your password has been changed successfully");
             }
+
             else
             {
-
-                fprintf(fptr2, "%s %s %s %s %s %s\n", firstname, lastname, email_address, username, phonenumber, password);
+                printf("Pssword could not changed");
             }
         }
-
-        fclose(fptr2);
-        fclose(fptr);
-
-        if (inside)
-        {
-            remove("users.txt");
-            rename("user_temp.txt", "users.txt");
-            printf("Your password has been changed successfully");
-        }
-
-        else
-        {
-            printf("Pssword could not changed");
-        }
+        break;
+    default:
+        break;
     }
 }
 
@@ -162,16 +181,64 @@ void delete_account()
     }
 }
 
+void show_data(){
+    FILE *fptr;
+    fptr = fopen("users.txt", "r");
+    
+    char username[20];
+    char password[20];
+    char firstname[20], lastname[20], email_address[100], phonenumber[20];
+    char new_password[20];
+    char input_email[100];
+    char del_account[1];
+    printf("Do you want to see your saved data(y/n): ");
+    scanf("%s", del_account);
+    int inside = 0;
+
+    if (del_account[0] == 'y' || del_account[0] == 'Y')
+    {
+        printf("Enter your email address to verify: ");
+        scanf("%s", input_email);
+
+        while (fscanf(fptr, "%s %s %s %s %s %s", firstname, lastname, email_address, username, phonenumber, password) == 6)
+        {
+            if (strcmp(email_address, input_email) == 0)
+            {
+                printf("Name: %s %s\n", firstname, lastname);
+                printf("Email Address: %s\n", email_address);
+                printf("Username: %s\n", username);
+                printf("Phone number: %s\n", phonenumber);
+                printf("Password: %s\n", password);
+                inside= 1;
+            }
+            
+        }
+
+        if (inside)
+        {
+            printf("Rewiew your data\n");
+        }
+        else
+        {
+            printf("Couldn't perform the operation!\n");
+        }
+        
+        fclose(fptr);
+
+    }
+
+}
+
 int main()
 {
     int i = 1;
     while (i)
     {
         int choice;
-        printf("\n1. Signup\n");
-        printf("2. Login\n");
-        printf("3. Forgot Password\n");
-        printf("4. Delete Account\n");
+        printf("1. Signup\n");
+        printf("2. Login / Reset Password\n");
+        printf("3. Delete Account\n");
+        printf("4. Show Data\n");
         printf("Enter your choice(0 to exit): ");
         scanf("%d", &choice);
         printf("Enter any key to continue.....................");
@@ -277,15 +344,9 @@ int main()
             break;
 
         case 2:
-            char enter_username[50];
-            char enter_password[20];
-            printf("Enter your username: ");
-            scanf("%s", enter_username);
-            printf("Enter your password: ");
-            scanf("%s", enter_password);
-            signin_function(enter_username, enter_password);
+            signin_function();
             printf("Enter any key to continue.....................");
-            scanf("%c", &cont);
+            scanf(" %c", &cont);
             if (cont)
             {
                 system("cls");
@@ -294,18 +355,18 @@ int main()
             break;
 
         case 3:
-            forgot__reset_password();
+            delete_account();
             printf("Enter any key to continue.....................");
-            char cont;
             scanf(" %c", &cont);
             if (cont)
             {
                 system("cls");
             }
+            
             break;
         
         case 4:
-            delete_account();
+            show_data();
             printf("Enter any key to continue.....................");
             scanf(" %c", &cont);
             if (cont)
